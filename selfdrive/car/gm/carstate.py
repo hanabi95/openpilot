ad359b6a98d59d045d20c0ac43dcd56b74317fbc
Jason Rapp <jasrapp@gmail.com>
2021-03-05 09:54:41 -0600

    try moving enabled into else

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -63,7 +63,8 @@ class CarState(CarStateBase):
       ret.cruiseState.available = not bool(pt_cp.vl["ECMEngineStatus"]['CruiseMainOn'])
     else:
       ret.cruiseState.available = bool(pt_cp.vl["ECMEngineStatus"]['CruiseMainOn'])
-    ret.cruiseState.enabled = self.pcm_acc_status != AccState.OFF
+      ret.cruiseState.enabled = self.pcm_acc_status != AccState.OFF # Only set cruisestate enabled
+      # if we're using the built in CC, otherwise we want OP to manage cruise state enable?
     print(f'buttons:{ret.cruiseState.enabled},{ret.cruiseState.available}')
     ret.cruiseState.standstill = self.pcm_acc_status == AccState.STANDSTILL
 
